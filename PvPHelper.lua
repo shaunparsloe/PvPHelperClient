4cfe33cd303dc0f985dd7c9521ef960189aa7a80
Shaun Parsloe <shaunparsloe@hotmail.com>
2014-09-18 09:07:40 +0100

    Changed Tab indents
    Increased indents from two spaces to one TAB

--- PvPHelper.lua
+++ PvPHelper.lua
@@ -2,62 +2,61 @@ PvPHelper = {}
 PvPHelper.__index = PvPHelper; -- failed table lookups on the instances should fallback to the class table, to get methods
 
 GVAR = {};
-GVAR.UpdateInterval = 1.0; -- How often the OnUpdate code will run (in seconds)
 
 
 function PvPHelper.new (options)
-  local self = setmetatable({}, PvPHelper)
-  self.AllCCTypes = CCTypeList:LoadAllCCTypes();
-  self.Message = deepcopy(Message.new());
+	local self = setmetatable({}, PvPHelper)
+	self.AllCCTypes = CCTypeList:LoadAllCCTypes();
+	self.Message = deepcopy(Message.new());
 	self.Message.ReceivePrefix = "PvPHelperClient";
 
-  self.CCTypes = self:MyCCTypes();
-  self.SpellsOnCooldown = CCTypeList.new();
-  self.MainAssit = nil;
-  self.CCTarget = nil;
-  self.InCombat = false;
-  self.MyName = UnitName("player").."-"..GetRealmName();
-  self.UI = PvPHelper_UI.new(self);
-  print("DEBUG: PVPHELPER: Setting MainFrame.PvPHelper to self");
-  return self;
+	self.CCTypes = self:MyCCTypes();
+	self.SpellsOnCooldown = CCTypeList.new();
+	self.MainAssit = nil;
+	self.CCTarget = nil;
+	self.InCombat = false;
+	self.MyName = UnitName("player").."-"..GetRealmName();
+	self.UI = PvPHelper_UI.new(self);
+	print("DEBUG: PVPHELPER: Setting MainFrame.PvPHelper to self");
+	return self;
 end
 
 function PvPHelper:MyCCTypes()
-  local myCCTypes = CCTypeList.new();
-  
-  for i,cctype in ipairs(self.AllCCTypes) do
-    if IsPlayerSpell(cctype.SpellId) then
-      myCCTypes:Add(cctype)
-      print("DEBUG: PVPHELPER: My CCType "..cctype.CCName);
-    end
-  end
-  return myCCTypes
+	local myCCTypes = CCTypeList.new();
+	
+	for i,cctype in ipairs(self.AllCCTypes) do
+		if IsPlayerSpell(cctype.SpellId) then
+			myCCTypes:Add(cctype)
+			print("DEBUG: PVPHELPER: My CCType "..cctype.CCName);
+		end
+	end
+	return myCCTypes
 end
 
 function PvPHelper:MessageReceived(strPrefix, strMessage, strType, strSender)
 	print("DEBUG:PvPHelper:MessageReceived "..strMessage)
-  self.Message:Format(strPrefix, strMessage, strType, strSender)
-  --print(tostring(self.Message.Header));
-  if (self.Message.Header)=="WhatSpellsDoYouHave" then -- 0010 = What spells do you have
-    -- print("DEBUGPvPHelper: Been asked which spells I have, reply with a list of my spells");
-    self:SendMessage("MySpells", self.CCTypes:ListSpellIds())
-  elseif (self.Message.Header)=="SetCCTarget" then -- 0030 = Set CCTarget
-    self:SetCCTarget(self.Message.Body)
-  elseif (self.Message.Header)=="SetMainAssist" then -- 0040 = Set Main Assist
-    self:SetMainAssist(self.Message.Body)
-  elseif (self.Message.Header)=="PrepareToAct" then -- 0050 = PrepareToAct
-    self:PrepareToAct(self.Message.Body);
-  elseif (self.Message.Header)=="ActNow" then -- 0060 = DoActionNow
-    self:DoCCActionNow(self.Message.Body);
-  else
-  	print("PvPHelper:MessageReceived: Unknown message header: "..tostring(self.Message.Header));
-  end
+	self.Message:Format(strPrefix, strMessage, strType, strSender)
+	--print(tostring(self.Message.Header));
+	if (self.Message.Header)=="WhatSpellsDoYouHave" then -- 0010 = What spells do you have
+		-- print("DEBUGPvPHelper: Been asked which spells I have, reply with a list of my spells");
+		self:SendMessage("MySpells", self.CCTypes:ListSpellIds())
+	elseif (self.Message.Header)=="SetCCTarget" then -- 0030 = Set CCTarget
+		self:SetCCTarget(self.Message.Body)
+	elseif (self.Message.Header)=="SetMainAssist" then -- 0040 = Set Main Assist
+		self:SetMainAssist(self.Message.Body)
+	elseif (self.Message.Header)=="PrepareToAct" then -- 0050 = PrepareToAct
+		self:PrepareToAct(self.Message.Body);
+	elseif (self.Message.Header)=="ActNow" then -- 0060 = DoActionNow
+		self:DoCCActionNow(self.Message.Body);
+	else
+		print("PvPHelper:MessageReceived: Unknown message header: "..tostring(self.Message.Header));
+	end
 end
 
 function PvPHelper:SendMessage(strMessage, strTarget)
 	-- print("DEBUG: Sending message to server("..self.Message.From..") "..strMessage.." - "..strTarget);
 	if (self.Message.From) then -- can only reply to server messages
-  	--self.Message.Prefix = "NEWPvPHelper";
+		--self.Message.Prefix = "NEWPvPHelper";
 		self.Message:SendMessagePrefixed("PvPHelperServer", strMessage, strTarget, self.Message.From);
 	end
 end
@@ -68,23 +67,23 @@ end
 --Conditional depending on test/live
 --
 --function PvPHelper:SetCCTarget(guid)
---  self.CCTarget = guid
---  self.UI:SetCCButton(self.CCTarget)
+--	self.CCTarget = guid
+--	self.UI:SetCCButton(self.CCTarget)
 --end
 --
 --function PvPHelper:SetMainAssist(guid)
---  self.MainAssist = guid
---  UI_SetMainAssist(self.MainAssist)
+--	self.MainAssist = guid
+--	UI_SetMainAssist(self.MainAssist)
 --end
 
 function PvPHelper:PrepareToAct(spellId)
---	print("PVPHELPER: Asked to Prepare to  act on spellid"..spellId);
-  self.UI:PrepareToAct(spellId)
+--	print("PVPHELPER: Asked to Prepare to	act on spellid"..spellId);
+	self.UI:PrepareToAct(spellId)
 end
 
 function PvPHelper:DoCCActionNow(spellId)
 --	print("PVPHELPER: ACT NOW on spellid"..spellId);
-  self.UI:DoCCActionNow(spellId)
+	self.UI:DoCCActionNow(spellId)
 end
 
 
@@ -94,20 +93,20 @@ function PvPHelper:RegisterMainFrameEvents(frame)
 	frame:SetScript("OnUpdate", PVPHelper_OnUpdate)
 
 	frame:RegisterEvent("PLAYER_ENTERING_WORLD");
-  frame:RegisterEvent("CHAT_MSG_ADDON")
-  frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
-    frame:RegisterEvent("PLAYER_REGEN_DISABLED");
-      frame:RegisterEvent("PLAYER_REGEN_ENABLED");
+	frame:RegisterEvent("CHAT_MSG_ADDON")
+	frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
+		frame:RegisterEvent("PLAYER_REGEN_DISABLED");
+			frame:RegisterEvent("PLAYER_REGEN_ENABLED");
 
-  print ("PVPHELPER - setting script");
-  frame:SetScript("OnEvent", PvPHelper_OnEvent)
+	print ("PVPHELPER - setting script");
+	frame:SetScript("OnEvent", PvPHelper_OnEvent)
 
 end
 
 function PvPHelper_OnEvent(frame, event, ...)
 		local timestamp, Event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool, param15,
 		param16, param17, param18, param19, param20, param21, param22, param23=...
---  print("Event: "..tostring(event)..":"..tostring(Event))
+--	print("Event: "..tostring(event)..":"..tostring(Event))
 
 	local pvpHelper = frame.parent;
 	
@@ -115,8 +114,8 @@ function PvPHelper_OnEvent(frame, event, ...)
 		pvpHelper.InCombat = true;
 	elseif event == "PLAYER_REGEN_ENABLED" then
 		pvpHelper.InCombat = false;
-  elseif event == "PLAYER_ENTERING_WORLD" then
-    print("PVPHELPER IS IN THE HOUSE!!!")
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		print("PVPHELPER IS IN THE HOUSE!!!")
 	elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
 
 --	print("UNIT_SPELLCAST_SUCCEEDED!")
@@ -136,36 +135,36 @@ function PvPHelper_OnEvent(frame, event, ...)
 		
 		pvpHelper:SendMessage("SpellCoolDown", "SPELL-"..tostring(ccType.SpellId))
 --		pvpHelper:SendMessage23456("ThisSpellIsOnCooldown123456", tostring(ccType.SpellId))
---    self:SendMessage("MySpells", self.CCTypes:ListSpellIds())
+--		self:SendMessage("MySpells", self.CCTypes:ListSpellIds())
 
 	else
 		print("Not one of my CC Spells");
 	end
 --	pvpHelper:SendMessage("ThisSpellIsOnCooldown", tostring(pvpHelper.CCSpellId))
 	
-  elseif event == "CHAT_MSG_ADDON" then
+	elseif event == "CHAT_MSG_ADDON" then
 --	print("PvPHelperClient-MESSAGE RECEIVED with stamp "..timestamp.." - "..tostring(Event))
 	
-    if (timestamp == "PvPHelperClient") then
-      pvpHelper:MessageReceived(tostring(timestamp), tostring(Event), tostring(hideCaster), tostring(sourceGUID))
+		if (timestamp == "PvPHelperClient") then
+			pvpHelper:MessageReceived(tostring(timestamp), tostring(Event), tostring(hideCaster), tostring(sourceGUID))
 --	else
 --		print("PvpHelperClient ERROR Message Received with stamp "..timestamp)
-    end
+		end
 
-    
-  end
+		
+	end
 end
 
 function PVPHelper_OnUpdate(frame, elapsed)
 
-  
-  frame.TimeSinceLastUpdate = frame.TimeSinceLastUpdate + elapsed; 	
+	
+	frame.TimeSinceLastUpdate = frame.TimeSinceLastUpdate + elapsed; 	
 
-  if (frame.TimeSinceLastUpdate > GVAR.UpdateInterval) then
-    
-    -- If we have notified the server that it's on cooldown, but now it's available, tell the server.
-    
-    local pvpHelper = frame.parent;
+	if (frame.TimeSinceLastUpdate > 0.5) then
+		
+		-- If we have notified the server that it's on cooldown, but now it's available, tell the server.
+		
+		local pvpHelper = frame.parent;
 
 		
 		for i, spell in ipairs( pvpHelper.SpellsOnCooldown) do
@@ -181,7 +180,7 @@ function PVPHelper_OnUpdate(frame, elapsed)
 --			 DEFAULT_CHAT_FRAME:AddMessage("Spell is cooling down, wait " .. (start + duration - GetTime()) .. " seconds for the next one.");
 			else
 --			 DEFAULT_CHAT_FRAME:AddMessage("Spell is ready.");
-			 pvpHelper.SpellsOnCooldown:Delete(spell);
+				pvpHelper.SpellsOnCooldown:Delete(spell);
 			 --pvpHelper:SendMessage("ThisSpellIsOffCooldown", tostring(spell.SpellId))
 			end
 			
@@ -193,35 +192,9 @@ function PVPHelper_OnUpdate(frame, elapsed)
 			
 			
 		end
---    -- Do we have a spell that we're told to cast?
---    if pvpHelper.CCSpellId then
---      usable, nomana = IsUsableSpell(pvpHelper.CCSpellId);
---      if (not usable) then
---      -- Can we not cast that spell?
---        if (not nomana) then
---          --print("The spell cannot be cast");
---          -- Have we notified the server that it cant be cast
---          if not pvpHelper.SpellsOnCooldown[pvpHelper.CCSpellId] then
---            pvpHelper:SendMessage("ThisSpellIsOnCooldown", tostring(pvpHelper.CCSpellId))
---            -- After notifying server, add it to the lists of spells on cooldown.
---            pvpHelper.SpellsOnCooldown[pvpHelper.CCSpellId] = "OnCooldown";
---          end  
---        else
---          --print("You do not have enough mana to cast the spell");
---        end
---      else
---        --print("The spell may be cast");
---        -- Have we notified the server that it cant be cast
---        if pvpHelper.SpellsOnCooldown[pvpHelper.CCSpellId] then
---          pvpHelper:SendMessage("ThisSpellIsOffCooldown", tostring(pvpHelper.CCSpellId))
---          -- After notifying server, clear it from the lists of spells on cooldown.
---          pvpHelper.SpellsOnCooldown[pvpHelper.CCSpellId] = nil;
---        end  
---      end
---    end
-  
-	  frame.TimeSinceLastUpdate = 0;
-  end
+	
+		frame.TimeSinceLastUpdate = 0;
+	end
 end
 
 
